141a05a2186f99e41283cd655c273755a6b7a124
xieguigang <xie.guigang@gcmodeller.org>
2021-11-13 22:30:17 +0800

    parse tree

--- src/Explorer/PathwayNavigator.ts
+++ src/Explorer/PathwayNavigator.ts
@@ -1,6 +1,23 @@
 namespace PathwayNavigator {
 
     export function parseJsTree(kegg_tree: KEGG.brite.IKEGGBrite): jsTree {
+        const name = KEGG.brite.parseIDEntry(kegg_tree.name);
+        const commonName = name.commonName || "KEGG";
 
+        if (isNullOrEmpty(kegg_tree.children)) {
+            return <jsTree>{
+                id: name.id,
+                text: commonName,
+                children: null
+            }
+        } else {
+            return <jsTree>{
+                id: name.id,
+                text: commonName,
+                children: $from(kegg_tree.children)
+                    .Select(parseJsTree)
+                    .ToArray()
+            }
+        }
     }
 }
