30463d76d86303956a90940e6a4cf2408391dc8d
xieguigang <xie.guigang@gcmodeller.org>
2021-11-14 08:39:51 +0800

    makes bugs fixed for the duplicated KO id

--- src/Explorer/PathwayNavigator.ts
+++ src/Explorer/PathwayNavigator.ts
@@ -1,18 +1,21 @@
 namespace PathwayNavigator {
 
+    let unique: number = 1;
+
     export function parseJsTree(kegg_tree: KEGG.brite.IKEGGBrite): jsTree {
         const name = KEGG.brite.parseIDEntry(kegg_tree.name);
         const commonName = name.commonName || "KEGG";
 
+        // makes bugs fixed for the duplicated KO id
         if (isNullOrEmpty(kegg_tree.children)) {
             return <jsTree>{
-                id: name.id,
+                id: `${name.id}_${++unique}`,
                 text: commonName,
                 children: null
             }
         } else {
             return <jsTree>{
-                id: name.id,
+                id: `${name.id}_${++unique}`,
                 text: commonName,
                 children: $from(kegg_tree.children)
                     .Select(parseJsTree)
