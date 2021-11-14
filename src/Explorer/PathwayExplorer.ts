3564e8e4b1272936b65392320f6be720fd323755
xieguigang <xie.guigang@gcmodeller.org>
2021-11-14 10:41:20 +0800

    Update PathwayExplorer.ts

--- src/Explorer/PathwayExplorer.ts
+++ src/Explorer/PathwayExplorer.ts
@@ -30,7 +30,7 @@ namespace apps {
 
             $(`#${target}`).jstree({
                 'core': {
-                    data: [tree]
+                    data: tree.children
                 }
             });
         }
