2d91bc6b6dd232dd42ba65c303f75f0151cacd4b
xieguigang <xie.guigang@gcmodeller.org>
2021-12-18 00:56:01 +0800

    make safe symbol

--- src/Editor/ModelPatch.ts
+++ src/Editor/ModelPatch.ts
@@ -2,25 +2,34 @@ namespace apps {
 
     const intId = /[-]?\d+/ig;
 
+    function makeSafeSymbol(name: string) {
+        return name
+            .replace(".", "_")
+            .replace("-", "_")
+            .replace("+", "_")
+            .replace("*", "_")
+            .replace("~", "_");
+    }
+
     export function ModelPatch(model: Model) {
         for (let node of model.nodeDataArray) {
             if (Strings.IsPattern(node.key.toString(), intId)) {
-                node.key = `T${node.key}`;
+                node.key = makeSafeSymbol(`T${node.key}`);
             }
         }
 
         for (let link of model.linkDataArray) {
             if (Strings.IsPattern(link.from.toString(), intId)) {
-                link.from = `T${link.from}`;
+                link.from = makeSafeSymbol(`T${link.from}`);
             }
             if (Strings.IsPattern(link.to.toString(), intId)) {
-                link.to = `T${link.to}`;
+                link.to = makeSafeSymbol(`T${link.to}`);
             }
 
             if (!isNullOrEmpty(link.labelKeys)) {
                 for (let i = 0; i < link.labelKeys.length; i++) {
                     if (Strings.IsPattern(link.labelKeys[i].toString(), intId)) {
-                        link.labelKeys[i] = `T${link.labelKeys[i]}`;
+                        link.labelKeys[i] = makeSafeSymbol(`T${link.labelKeys[i]}`);
                     }
                 }
             }
