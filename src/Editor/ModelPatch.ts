b4f248be536eae9ce8a0f195352aaf203085533f
xieguigang <xie.guigang@gcmodeller.org>
2021-12-18 00:51:03 +0800

    Update ModelPatch.ts

--- src/Editor/ModelPatch.ts
+++ src/Editor/ModelPatch.ts
@@ -19,7 +19,7 @@ namespace apps {
 
             if (!isNullOrEmpty(link.labelKeys)) {
                 for (let i = 0; i < link.labelKeys.length; i++) {
-                    if (Strings.IsPattern(link.labelKeys[i], intId)) {
+                    if (Strings.IsPattern(link.labelKeys[i].toString(), intId)) {
                         link.labelKeys[i] = `T${link.labelKeys[i]}`;
                     }
                 }
