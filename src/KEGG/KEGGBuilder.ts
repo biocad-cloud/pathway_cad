a3bce670d7ce674f9e8150ffd22206777f949c6d
xieguigang <xie.guigang@gcmodeller.org>
2022-01-10 21:56:25 +0800

    how to search kegg component?

---
+++ src/KEGG/KEGGBuilder.ts
@@ -0,0 +1,13 @@
+namespace apps {
+
+    export class KEGGNetwork extends Bootstrap {
+
+        public get appName(): string {
+            return "kegg_network";
+        }
+
+        protected init(): void {
+            throw new Error("Method not implemented.");
+        }
+    }
+}
